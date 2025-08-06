f25c85b6c6aa66c97f543038f82743f4a54bffdd
Auth0 bugbounty POC by @nvk0x <70015070+auth0-community@users.noreply.github.com>
2025-08-06 16:06:56 +0530

    Update auth0.go

--- auth0.go
+++ auth0.go
@@ -3,5 +3,5 @@ package auth0
 import "fmt"
 
 func init() {
-    fmt.Println("[POC] Code execution from hijacked github.com/auth0-community/go-auth0")
+    fmt.Println("[POC] Code execution from hijacked github.com/auth0-community/auth0")
 }
